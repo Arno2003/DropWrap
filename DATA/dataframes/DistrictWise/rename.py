679c72d4279a9ff637697a6c9b8304167c648aed
Hindol19 <hindolbanerjee5@gmail.com>
2024-01-27 16:54:06 +0530

    Added test df for gujarat

--- DATA/dataframes/DistrictWise/rename.py
+++ DATA/dataframes/DistrictWise/rename.py
@@ -1,17 +1,19 @@
 import os
+import pandas as pd
 year = "2021-2022"
 folder_path = "DATA/dataframes/DistrictWise/"+year
+# for filename in os.listdir(folder_path):
+#     temp = filename.split('_')[-2]
+
+#     new_file_name = temp.split('   ')[0]+".xlsx"
+
+#     # # Rename the file
+#     os.rename("DATA/dataframes/DistrictWise/"+year+"/" +
+#               filename, "DATA/dataframes/DistrictWise/"+year+"/"+new_file_name)
+
+#     print(f'The file has been renamed to: {new_file_name}')
+
 for filename in os.listdir(folder_path):
-    # original_file_name = "Dropout Rate by Gender, Level of School Education and Social Category_State Name _Andaman & Nicobar Islands District - All District_22.xlsx"
-
-    # print(filename)
-
-    # Extract the desired part of the file name
-    temp = filename.split('_')[-2]
-    new_file_name = temp.split('   ')[0]+".csv"
-
-    # # Rename the file
-    os.rename("DATA/dataframes/DistrictWise/"+year+"/" +
-              filename, "DATA/dataframes/DistrictWise/"+year+"/"+new_file_name)
-
-    print(f'The file has been renamed to: {new_file_name}')
+    df = pd.read_excel(folder_path+"/Gujarat.xlsx")
+    print(df)
+    # df.to_csv("DATA/dataframes/DistrictWise/"+year+"/Gujarat")
