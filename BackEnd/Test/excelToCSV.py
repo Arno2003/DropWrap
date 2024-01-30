85889d1b7333d8c5f49c7313b8bfee46d43c025c
Arno2003 <arnabkarate@gmail.com>
2024-01-30 22:19:56 +0530

    testing....

--- BackEnd/Test/excelToCSV.py
+++ BackEnd/Test/excelToCSV.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Specify the Excel file path and the number of rows to skip
-excel_file = 'D:\projects\SIH\Smart-India-Hackathon\BackEnd\Test\DropoutRateAndhraPradesh.xlsx'
+excel_file = 'BackEnd\Test\Dropout Rate by Gender, Level of School Education and Social Category_State Name _Bihar   District - All District_21.xlsx'
 n_rows_to_skip = 3  # Adjust this value as needed
 
 # Read the Excel file into a DataFrame, skipping the specified rows
 df = pd.read_excel(excel_file, skiprows=n_rows_to_skip)
 
 # Specify the desired CSV output file path
-csv_file = 'D:\projects\SIH\Smart-India-Hackathon\BackEnd\Test\output_data_2019-20.csv'
+csv_file = 'BackEnd\Test\Bihar20-2021.csv'
 
 # Save the DataFrame to a CSV file
 df.to_csv(csv_file)
