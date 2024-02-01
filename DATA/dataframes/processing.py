5dd8c3b912e915355d42db04abd35adcce410014
Arno2003 <arnabkarate@gmail.com>
2024-02-01 18:11:02 +0530

    ..

--- DATA/dataframes/processing.py
+++ DATA/dataframes/processing.py
@@ -4,8 +4,12 @@ def clean_data(csv_file_path, cleaned_csv_file_path):
     df = pd.read_csv(csv_file_path)
     for column in df.columns:
         if df[column].dtype == 'object':
-            df[column] = df[column].str.rstrip(' \t\n\r')  # Strip trailing spaces and special characters
-
+            df[column] = df[column].str.rstrip(' \t\n\r *')  # Strip trailing spaces and special characters
+            df[column] = df[column].apply(lambda x: ''.join(filter(str.isalpha, str(x))))
+            df[column] = df[column].str.upper()
+            
+            # print(df[column].str[-1])
+            
     # Save the cleaned DataFrame to a new CSV file
     df.to_csv(cleaned_csv_file_path, index=False)
 
