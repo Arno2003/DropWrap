871e9d3a58cc099b8c1a62bbc82804983d16497b
Hindol19 <hindolbanerjee5@gmail.com>
2024-02-01 16:58:32 +0530

    Converted data of 20-21 and 21-22 to csv

--- DATA/dataframes/DistrictWise/rename.py
+++ DATA/dataframes/DistrictWise/rename.py
@@ -2,18 +2,12 @@ import os
 import pandas as pd
 year = "2021-2022"
 folder_path = "DATA/dataframes/DistrictWise/"+year
-# for filename in os.listdir(folder_path):
-#     temp = filename.split('_')[-2]
-
-#     new_file_name = temp.split('   ')[0]+".xlsx"
-
-#     # # Rename the file
-#     os.rename("DATA/dataframes/DistrictWise/"+year+"/" +
-#               filename, "DATA/dataframes/DistrictWise/"+year+"/"+new_file_name)
-
-#     print(f'The file has been renamed to: {new_file_name}')
-
 for filename in os.listdir(folder_path):
-    df = pd.read_excel(folder_path+"/Gujarat.xlsx")
-    print(df)
-    # df.to_csv("DATA/dataframes/DistrictWise/"+year+"/Gujarat")
+    temp = filename.split('_')[-2]
+
+    new_file_name = temp.split('   ')[0]+".xlsx"
+
+    os.rename("DATA/dataframes/DistrictWise/"+year+"/" +
+              filename, "DATA/dataframes/DistrictWise/"+year+"/"+new_file_name)
+
+    print(f'The file has been renamed to: {new_file_name}')
