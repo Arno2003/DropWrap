ccdfbe239c3981b37686eaf1627b40890f448a38
Arno2003 <arnabkarate@gmail.com>
2024-02-03 00:59:51 +0530

    .

--- mergeCoordClust.py
+++ mergeCoordClust.py
@@ -6,7 +6,7 @@ df2 = pd.read_csv('BackEnd\Test\castClustGuj.csv')[['Location', 'cluster']]
 
 # Merge the DataFrames based on the common column
 merged_df = pd.merge(df1, df2, on='Location', how='inner')
-
+merged_df.drop_duplicates()
 # 'inner' indicates that only rows with common values in both DataFrames will be included in the result.
 # If you want to include all rows from both DataFrames, you can use 'outer' instead of 'inner'.
 
