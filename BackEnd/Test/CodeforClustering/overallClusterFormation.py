4a723c0cc2e0af5ea18cb818d93659161223c995
arno2003 <arnabkarate@gmail.com>
2024-03-17 22:09:08 +0530

    modification going on

--- BackEnd/Test/CodeforClustering/overallClusterFormation.py
+++ BackEnd/Test/CodeforClustering/overallClusterFormation.py
@@ -5,6 +5,32 @@ import os
 
 # merging at the same time while forming, to create a final combined file
 
+def combineCSV(folderPath):
+    par = ['General', 'OBC', 'Overall', 'SC', 'ST']
+
+    combined_output = pd.DataFrame()
+
+    for i in range(0, len(par)):
+        combined_data = pd.DataFrame()
+        for filename in os.listdir(folderPath):
+            if filename.endswith(".csv"):
+
+                params = filename.split('(')[1].split(')')[0].split('_')
+                print(params)
+                cat, std, gen = params[0], params[1], params[2]
+                if par[i] == cat:
+                    file_path = os.path.join(folderPath, filename)
+
+                    # Read the CSV file into a dataframe
+                    df = pd.read_csv(file_path)
+
+                    combined_data['Location'] = df['Location']
+                    combined_data['Caste'] = df['Caste']
+                    combined_data[params[1]+'_'+params[2]] = df['Cluster']
+
+        combined_output = pd.concat(
+            [combined_output, combined_data], ignore_index=True)
+        combined_output.to_csv(f"DATA\\dataframes\\formatted_cluster_data.csv")
 
 
 
@@ -51,9 +77,19 @@ def formClusterCasteWise(folderPath):
             # Save the result to a new CSV file
             result_df.to_csv(f'BackEnd\\Test\\OutputData\\filteredOutputs\\clusters\\{fileName}.csv', index=False)
 
-
+            # Saving the cluster details in another CSV file
             cluster_df = pd.DataFrame(Z, columns=['Cluster 1', 'Cluster 2', 'Distance', 'Number of Points'])
             cluster_df.to_csv(f'BackEnd\\Test\\OutputData\\filteredOutputs\\clusterDetails\\{fileName}_details.csv', index=False)
+
+def formClusterCSV(folderPath):
+    try:
+        if os.path.isdir(folderPath):
+            formClusterCasteWise(folderPath)
+            combineCSV(folderPath)
+    except OSError:
+        print("Invalid Directory Path")
+        return False
+        
 
 if __name__ == "__main__":
     folderPath = "BackEnd\Test\InputData\FilteredData"
