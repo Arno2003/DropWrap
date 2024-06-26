a4508e24305dea4523ac9ccd33394b2a8855dbe1
Debanwesa2003 <debanwesa@gmail.com>
2024-06-27 01:20:50 +0530

    commit

--- BackEnd/database/df_to_db.py
+++ BackEnd/database/df_to_db.py
@@ -10,12 +10,12 @@ password = os.getenv("MONGO_PASSWORD")
 client = pymongo.MongoClient(
     f"mongodb+srv://dropwrap:{password}@gujarat.jwam9ab.mongodb.net/?retryWrites=true&w=majority&appName=Gujarat")
 
-df = pd.read_csv("BackEnd\\database\\Gujarat.csv")
+df = pd.read_csv("BackEnd\\database\\formatted_cluster_data.csv")
 
 # print(df.head())
 
 data = df.to_dict(orient="records")
 
-db = client['Dropout']
+db = client['Cluster_No']
 
-db.Dropout_Rates.insert_many(data)
+db.Cluster_nos.insert_many(data)
