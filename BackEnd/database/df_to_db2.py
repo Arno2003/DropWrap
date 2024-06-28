c6b49f9e27ac6548083854e3256ae0ecf70e365d
Debanwesa2003 <debanwesa@gmail.com>
2024-06-28 23:17:42 +0530

    commit

--- BackEnd/database/df_to_db2.py
+++ BackEnd/database/df_to_db2.py
@@ -8,18 +8,53 @@ load_dotenv()
 password = os.getenv("MONGO_PASSWORD_NEW")
 
 client = pymongo.MongoClient(
-    f"mongodb+srv://debanwesa_bandhu:{password}@cluster0.u5akrs9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-path="BackEnd\\database\\Gujarat"
-dir_list=os.listdir(path)
+    f"mongodb+srv://hindol_banerjee:{password}@cluster0.u5akrs9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# path="BackEnd\\database\\Gujarat"
+# dir_list=os.listdir(path)
 #print("files in gujarat are")
 #print(dir_list)
 
-for file in dir_list:
+# for file in dir_list:
+#      try:
+#         df = pd.read_csv(f"BackEnd\\database\\Gujarat\\{file}")
 
-    df = pd.read_csv(f"BackEnd\\database\\Gujarat\\{file}")
+#         data = df.to_dict(orient="records")
 
-    data = df.to_dict(orient="records")
+#         db = client['Gujarat']
+#         collection=db[file]
+#         collection.insert_many(data)
+#      except Exception as e:
+#          print(e)
 
-    db = client['Gujarat']
-    collection=db[file]
-    db.collection.insert_many(data)
+# df = pd.read_csv("BackEnd\\database\\Gujarat\\formatted_cluster_data.csv")
+
+# data = df.to_dict(orient="records")
+
+# db = client['Gujarat']
+
+# db.formatted_cluster_data.insert_many(data)
+
+df = pd.read_csv("BackEnd\\database\\Gujarat\\latlong.csv")
+
+data = df.to_dict(orient="records")
+
+db = client['Gujarat']
+
+db.latlong.insert_many(data)
+
+df = pd.read_csv("BackEnd\\database\\Gujarat\\rates.csv")
+
+data = df.to_dict(orient="records")
+
+db = client['Gujarat']
+
+db.rates.insert_many(data)
+
+df = pd.read_csv("BackEnd\\database\\Gujarat\\reasons.csv")
+
+data = df.to_dict(orient="records")
+
+db = client['Gujarat']
+
+db.reasons.insert_many(data)
+
