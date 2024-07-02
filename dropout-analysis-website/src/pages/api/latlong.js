411c93c6a9d06d595b75744bc5bba07ca8fae504
Hindol19 <hindolbanerjee5@gmail.com>
2024-07-03 01:33:39 +0530

    plotted map using fetched data

--- dropout-analysis-website/src/pages/api/latlong.js
+++ dropout-analysis-website/src/pages/api/latlong.js
@@ -4,11 +4,14 @@ export default async (req, res) => {
   try {
     console.log(req.query.dbName);
     const dbName = req.query.dbName || "Gujarat";
+    // console.log(req.query);
     const client = await clientPromise;
     const db = client.db(dbName);
     const result = await db
       .collection("latlong")
-      .find({})
+      .find({
+        Social_Category: req.query.caste,
+      })
       .sort({ metacritic: -1 })
       .toArray();
     res.json(result);
