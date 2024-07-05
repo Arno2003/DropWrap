cb6217638c412fa96782040bec027ae7dd7f4cc6
Hindol19 <hindolbanerjee5@gmail.com>
2024-07-06 02:47:55 +0530

    updated merge.py

--- BackEnd/DATA/merge.py
+++ BackEnd/DATA/merge.py
@@ -1,20 +1,40 @@
+import os
 import pandas as pd
 
 
-def preprocess(df):
+def preprocessLatLon(df):
     df = df.drop(df.columns[0], axis=1)
+    df.rename(columns={'District': 'Location'}, inplace=True)
+
     for i, row in df.iterrows():
         lat = row[1].split()
         lon = row[2].split()
         lat1 = float(lat[0][0:2]+'.'+lat[1][0:-1])
         lon1 = float(lon[0][0:2]+'.'+lon[1][0:-1])
 
+        df.at[i, 'Location'] = row[0].upper()
         df.at[i, 'Latitude'] = lat1
         df.at[i, 'Longitude'] = lon1
 
-    print(df.head())
+    return df
 
 
-df = pd.read_csv("BackEnd\\DATA\\latlong_dataset\\Andhrapradesh.csv")
+def mergeDF(latlon, rates, targetPath):
+    # for i, rate in rates.iterrows():
+    #     for j, latlon in latlon.iterrows():
+    #         print(rate['Location'] == latlon['Location'])
+    df_merged = pd.merge(latlon, rates, on='Location')
+    df_merged.to_csv(targetPath)
 
-preprocess(df=df)
+
+latlon = pd.read_csv("BackEnd\\DATA\\latlong_dataset\\Andhra Pradesh.csv")
+rates = pd.read_csv(
+    "DATA\Test\DistrictWiseData\Andhra Pradesh.csv")
+
+latlon = preprocessLatLon(df=latlon)
+
+directory = "BackEnd\\database\\Andhra Pradesh"
+os.makedirs(directory)
+
+targetPath = directory+"\\Andhra Pradesh.csv"
+mergeDF(latlon=latlon, rates=rates, targetPath=targetPath)
