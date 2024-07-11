1b00f375df52a53247a2fa303b34920667da18be
arno2003 <arnabkarate@gmail.com>
2024-07-11 21:55:54 +0530

    garbage disposal

--- DATA/dataframes/DistrictWise/Coordinates/cleanup.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-folder_path = "DATA/dataframes/DistrictWise/Coordinates"
-
-df = pd.read_csv(folder_path+"/DistrictCd.csv")
-
-print(df['District'].head(50))
-df['District'] = df['District'].str.replace(' \*', '')
-print(df['District'].head(50))
