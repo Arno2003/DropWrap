1c0ff354a8057a5c3f1c224761d7063f79d2a600
arno2003 <arnabkarate@gmail.com>
2024-07-22 20:59:52 +0530

    sigmoid added

--- BackEnd/Test/RNNCode/softmax/softMax.py
+++ BackEnd/Test/RNNCode/softmax/softMax.py
@@ -43,7 +43,7 @@ def impression(data: pd.DataFrame, colName: str):
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Train the model
-    history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+    history = model.fit(X_train, y_train, epochs=60, batch_size=32, validation_split=0.2)
 
     # Evaluate the model
     loss, accuracy = model.evaluate(X_test, y_test)
